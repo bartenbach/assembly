eebf36e4d183915192ce4893f22eec6f6e08f4ae
Blake Bartenbach <blakebartenbach@gmail.com>
2016-11-08 16:31:50 -0600

    Cleaner, better

--- ChessBoard.asm
+++ ChessBoard.asm
@@ -7,76 +7,56 @@ WaitMsg       PROTO
 WriteChar     PROTO
 SetTextColor  PROTO
 CrLf          PROTO
+WriteEvenRow  PROTO times:byte
+WriteOddRow   PROTO times:byte
+WriteSquare   PROTO color:byte
 
 .data
     whiteSquare = white + (white * 16)
     graySquare  = gray  + (gray  * 16)
     default     = white + (black * 16)
+    BOARDSIZE   = 4
 
 .code
     main PROC
         mov al,' '  ; our chess board character
-        call WriteEvenRow
-        call CrLf
-        call WriteOddRow
-        call CrLf
-        call WriteEvenRow
-        call CrLf
-        call WriteOddRow
-        call CrLf
-        call WriteEvenRow
-        call CrLf
-        call WriteOddRow
-        call CrLf
-        call WriteEvenRow
-        call CrLf
-        call WriteOddRow
-        call CrLf
+        mov ecx,BOARDSIZE ; times to write even and odd rows
+        WriteBoard:
+            invoke WriteEvenRow, 4
+            invoke WriteOddRow,  4
+        LOOP WriteBoard
         call RestoreDefaultColor
         call WaitMsg
-
         exit
     main ENDP
 
-    WriteEvenRow PROC
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
+    WriteEvenRow PROC uses ecx, times:byte
+        movzx ecx,times
+        OutputEven:
+            invoke WriteSquare, whiteSquare
+            invoke WriteSquare, graySquare
+        LOOP OutputEven
+        call CrLf
         ret
     WriteEvenRow ENDP
 
-    WriteOddRow PROC
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
-        call WriteGraySquare
-        call WriteWhiteSquare
+    WriteOddRow PROC uses ecx, times:byte
+        movzx ecx,times
+        OutputOdd:
+            invoke WriteSquare, graySquare
+            invoke WriteSquare, whiteSquare
+        LOOP OutputOdd
+        call CrLf
         ret
     WriteOddRow ENDP
 
-    WriteWhiteSquare PROC
-        mov eax,whiteSquare
+    WriteSquare PROC color:byte
+        movzx eax,color
         call SetTextColor
         call WriteChar
         call WriteChar
         ret
-    WriteWhiteSquare ENDP
-
-    WriteGraySquare PROC
-        mov eax,graySquare
-        call SetTextColor
-        call WriteChar
-        call WriteChar
-        ret
-    WriteGraySquare ENDP
+    WriteSquare ENDP
 
     RestoreDefaultColor PROC
        mov eax,default
