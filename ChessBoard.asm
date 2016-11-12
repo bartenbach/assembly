9461589e3309ddee4ea46deb3dbccfef10cb0730
Blake Bartenbach <blakebartenbach@gmail.com>
2016-11-11 22:15:18 -0600

    Added color variations

--- ChessBoard.asm
+++ ChessBoard.asm
@@ -1,65 +1,98 @@
 TITLE ChessBoard
 ; Blake Bartenbach
-; Chapter 8 - Exercise 2
-;  
+; Chapter 8 - Exercise 3
+; Chess board that loops through colors
+
 INCLUDE C:\Irvine\Irvine32.inc
 WaitMsg       PROTO
 WriteChar     PROTO
 SetTextColor  PROTO
 CrLf          PROTO
-WriteEvenRow  PROTO times:byte
-WriteOddRow   PROTO times:byte
-WriteSquare   PROTO color:byte
+Delay         PROTO
+Clrscr        PROTO
+WriteEvenRow  PROTO times:byte, color:dword
+WriteOddRow   PROTO times:byte, color:dword
+WriteSquare   PROTO color:dword
+WriteBoard    PROTO color:dword
+UpdateColor   PROTO
 
 .data
-    whiteSquare  = white + (white * 16)
-    graySquare   = gray  + (gray  * 16)
+    whiteSquare  = 15 + (15 * 16)
     default      = white + (black * 16)
     BOARDROWS    = 8
     BOARDCOLUMNS = 8
+    LOOPS        = 16
+    count byte 0 ; if this isn't global the program breaks for some reason
 
 .code
     main PROC
-        mov al,' '  ; our chess board character
-        mov ecx,BOARDCOLUMNS / 2 ; times to write even and odd rows
-        WriteBoard:
-            invoke WriteEvenRow, BOARDROWS / 2
-            invoke WriteOddRow,  BOARDROWS / 2
-        LOOP WriteBoard
+        mov ecx,loops
+        mov edx,0 ; first board color
+
+        WriteBoards:
+            invoke WriteBoard, edx
+            inc count
+            call UpdateColor
+
+            mov eax,500 ; milliseconds to delay
+            call Delay
+            call Clrscr
+        LOOP WriteBoards
         call RestoreDefaultColor
         call WaitMsg
         exit
     main ENDP
 
-    WriteEvenRow PROC uses ecx, times:byte
+    ; receives: count - counter variable
+    ; returns: color in edx
+    UpdateColor PROC
+        mov edx,0
+        movzx edx,count
+        imul edx,16
+        ret
+    UpdateColor ENDP
+
+    WriteBoard PROC uses ecx, color:dword
+            mov ecx,BOARDCOLUMNS / 2 ; times to write even and odd rows
+        Output:
+            invoke WriteEvenRow, BOARDROWS / 2, color
+            invoke WriteOddRow,  BOARDROWS / 2, color
+        LOOP Output
+        call RestoreDefaultColor
+        ret
+    WriteBoard ENDP
+
+    WriteEvenRow PROC uses ecx, times:byte, color:dword
         movzx ecx,times
         OutputEven:
             invoke WriteSquare, whiteSquare
-            invoke WriteSquare, graySquare
+            invoke WriteSquare, color
         LOOP OutputEven
+        call RestoreDefaultColor
         call CrLf
         ret
     WriteEvenRow ENDP
 
-    WriteOddRow PROC uses ecx, times:byte
+    WriteOddRow PROC uses ecx, times:byte, color:dword
         movzx ecx,times
         OutputOdd:
-            invoke WriteSquare, graySquare
+            invoke WriteSquare, color
             invoke WriteSquare, whiteSquare
         LOOP OutputOdd
         call CrLf
         ret
     WriteOddRow ENDP
 
-    WriteSquare PROC color:byte
-        movzx eax,color
+    WriteSquare PROC uses eax, color:dword
+        mov eax,color
         call SetTextColor
+        mov al,' '
         call WriteChar
         call WriteChar
         ret
     WriteSquare ENDP
 
-    RestoreDefaultColor PROC
+    RestoreDefaultColor PROC uses eax
        mov eax,default
        call SetTextColor
        ret
